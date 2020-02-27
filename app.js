7cd28b4aab1041ac3c8a8ca5742d326d19f98965
jccuevas <jccuevas@ujaen.es>
2020-02-27 18:23:26 +0100

    Configurando Express

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ var port = 8080;
 
 app.set('views', './views');
 app.set('view engine', 'pug');
+app.use(express.static('./public'));
 
 app.get("/",function(req,res){
     res.render('index', { title: 'Hey', message: 'Hello there!'});
