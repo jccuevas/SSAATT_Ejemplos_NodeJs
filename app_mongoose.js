10e74af62640b757d046f54d2b0a982e0e7aa68d
jccuevas <jccuevas@ujaen.es>
2020-05-02 18:17:56 +0200

    Preparando ejemplos. - Ejemplo de Mongoose

--- app_mongoose.js
+++ app_mongoose.js
@@ -12,17 +12,11 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
 mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true});
-
 const hostname = '127.0.0.1';
 const port = 3000;
-
 const app = express();
-
 app.use(express.static(__dirname + '/public'));
-
-
 // create application/json parser
 var jsonParser = bodyParser.json()
 
@@ -35,10 +29,7 @@ var userSchema = new Schema({
     apellido1: String,
     apellido2: String
 });
-
 const User = mongoose.model('User', userSchema);
-
-
 app.route("/user")
         .get((req, res) => {
             res.writeHead(200, {
@@ -49,25 +40,23 @@ app.route("/user")
                     res.write("<p>Error</p>");
                 } else {
                     for (let item of users) {
-                        res.write(`<p> ${item.nombre} ${item.apellido1}`);
+                        res.write(`<p> ${item.apellido1} ${item.apellido2}, ${item.nombre} `);
                     }
-
                 }
                 res.end("</body></html>");
-
             });
 
         })
         .post(urlencodedParser, (req, res) => {
             console.log("POST /user" + JSON.stringify(req.body));
-            let user = new User({nombre: req.body.nombre, apellido1: req.body.apellido1, apellido2: req.body.apellido2});
+            //let user = new User({nombre: req.body.nombre, apellido1: req.body.apellido1, apellido2: req.body.apellido2});
+            let user = new User(req.body);
             res.writeHead(200, {
                 'Content-Type': 'text/html'});
             res.write("<html><meta charset='utf8'><body>Hola método POST");
             user.save(function (err, user) {
                 if (err) {
                     res.write("<p>Error</p>");
-
                 } else {
                     res.write("<p>Grabado</p>")
                     for (let param in req.body) {
@@ -77,8 +66,6 @@ app.route("/user")
                 res.end("</body></html>");
             });
         });
-
-
 app.listen(port, hostname, function () {
     console.log('Ejemplo de app escuchando en el puerto 3000');
 });
